b18127a68314a05881441b1dbdf10dea87023fe6
Michael <michael@Admins-MacBook-Pro.local>
2021-11-20 23:03:19 -0500

    created new node struct, reads data from file and makes new structs

--- main.jl
+++ main.jl
@@ -1,3 +1,26 @@
 using LinearAlgebra;
 
-test test
+struct node
+  index::Int
+  name::String
+  neighbors::Vector{Int64}
+end
+
+open("graphs/test.txt") do io
+  n = parse(Int, readline(io))
+  for i in 1:n
+    line = readline(io)
+    data = split(line, " ")
+    
+    neighbors = Int[]
+    for j in 2:length(data)
+      push!(neighbors, parse(Int, data[j]))
+    end
+
+    newNode = node(i,data[1], neighbors)
+
+    println(newNode.index)
+    println(newNode.name)
+    println(newNode.neighbors)
+  end
+end
