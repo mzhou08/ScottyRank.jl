41d14514b351ebf2a0ef7d8ff43bc61baa291d5a
Michael <michael@Admins-MacBook-Pro.local>
2021-11-20 23:26:45 -0500

    added some comments

--- main.jl
+++ main.jl
@@ -1,5 +1,9 @@
 using LinearAlgebra;
 
+# Struct for a graph node.
+# Index: its unique integer index, ranging from 1 to n.
+# Name: the name we assign to the node, e.g. "Wikipedia"
+# Neighbors: the nodes that this node points to.
 struct node
   index::Int
   name::String
@@ -12,11 +16,16 @@ open("graphs/test.txt") do io
     line = readline(io)
     data = split(line, " ")
     
+    # Initializing the neighbors array
     neighbors = Int[]
+
+    # Constructing the list of neighbors
+    # data[i] is the name, so it is not included here
     for j in 2:length(data)
       push!(neighbors, parse(Int, data[j]))
     end
 
+    # Constructing a new node struct
     newNode = node(i,data[1], neighbors)
 
     println(newNode.index)
