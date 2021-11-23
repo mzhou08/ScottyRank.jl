246342a0c0b9e486bf03adaccd842bc353ac313d
Siyuan Daniel Chen <siyuanc2@andrew.cmu.edu>
2021-11-23 02:01:22 -0500

    combined two modules into main jl

--- src/ScottyRank.jl
+++ src/ScottyRank.jl
@@ -1,7 +1,34 @@
 module ScottyRank
 
-include("GraphModule.jl")
-include("IOModule.jl")
-export GraphModule, IOModule
+using DelimitedFiles
+
+export Vertex, Graph, read_edge_list
+
+struct Vertex
+  index::UInt32
+  in_neighbors::Vector{UInt32}
+  out_neighbors::Vector{UInt32}
+end
+
+struct Graph
+  num_vertices::UInt32
+  vertices::Vector{Vertex}
+end
+
+function read_edge_list(filepath::String)
+  file = open(filepath)
+  num_vertices, num_edges = map(x -> convert(UInt32, x), readdlm(IOBuffer(readline(file))))
+  vertices = Array{Vertex}(undef, num_vertices)
+  for i in 1:num_vertices
+    vertices[i] = Vertex(i, Array{UInt32}(undef, 0), Array{UInt32}(undef, 0));
+  end
+  for _ in 1:num_edges
+    index_from, index_to = map(x -> convert(UInt32, x), readdlm(IOBuffer(readline(file))))
+    push!(vertices[index_from].out_neighbors, index_to)
+    push!(vertices[index_to].in_neighbors, index_from)
+  end
+  close(file)
+  vertices
+end
 
 end
