196e7488f43c265972994fbe66c71d20faf61506
Siyuan Daniel Chen <siyuanc2@andrew.cmu.edu>
2021-11-23 10:29:53 -0500

    added read_from_adjacency_list

--- src/ScottyRank.jl
+++ src/ScottyRank.jl
@@ -2,7 +2,7 @@ module ScottyRank
 
 using DelimitedFiles
 
-export Vertex, Graph, read_edge_list
+export Vertex, Graph, read_edge_list, read_adjacency_list
 
 struct Vertex
   index::UInt32
@@ -24,6 +24,21 @@ function read_edge_list(filepath::String)
   end
   for _ in 1:num_edges
     index_from, index_to = map(x -> convert(UInt32, x), readdlm(IOBuffer(readline(file))))
+    push!(vertices[index_from].out_neighbors, index_to)
+    push!(vertices[index_to].in_neighbors, index_from)
+  end
+  close(file)
+  vertices
+end
+
+function read_adjacency_list(filepath::String)
+  file = open(filepath)
+  num_vertices = parse(UInt32, readline(file))
+  vertices = Array{Vertex}(undef, num_vertices)
+  for i in 1:num_vertices
+    vertices[i] = Vertex(i, Array{UInt32}(undef, 0), Array{UInt32}(undef, 0));
+  end
+  for index_from in 1:num_vertices, index_to in map(x -> convert(UInt32, x), readdlm(IOBuffer(readline(file))))
     push!(vertices[index_from].out_neighbors, index_to)
     push!(vertices[index_to].in_neighbors, index_from)
   end
